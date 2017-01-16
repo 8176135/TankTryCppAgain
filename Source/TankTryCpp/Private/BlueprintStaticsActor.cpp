f0162534695a0255182eeef4d90f333e430fde67
Hand-of-Cthulhu <necrorerxx@gmail.com>
2017-01-17 10:03:09 +1300

    Almost Perfect Twice

---
+++ Source/TankTryCpp/Private/BlueprintStaticsActor.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTryCpp.h"
+#include "BlueprintStaticsActor.h"
+
+
+// Sets default values
+ABlueprintStaticsActor::ABlueprintStaticsActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABlueprintStaticsActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABlueprintStaticsActor::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
